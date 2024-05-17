ee3b220d76acc3feaa47366b046fefafce8eaf01
nathaniEL mujesia <nathanielmujesia@gmail.com>
2024-05-17 20:26:05 +0300

    routes and farmer's ui

--- src/components/Auth/CustomerSingUp.jsx
+++ src/components/Auth/CustomerSingUp.jsx
@@ -1,11 +1,14 @@
-import {Link} from "react-router-dom"
-function  CustomerSingUp () {
-  return (
-    <div>
-        <h1>CustomerSingUp</h1>
-        <Link to="/login"></Link>        
-    </div>
-  )
+import {NavLink} from "react-router-dom"
+
+function CustomerSingUp() {
+    return(
+        <div className="home-links flex">
+        <h1 className='font-bold text-4xl'>Customer</h1>
+            <NavLink to="/login" className="capitalize">log in</NavLink>
+            <NavLink  className="capitalize"to="/farmerSingUp">farmer sign up</NavLink>
+        </div>
+
+    ) 
 }
 
-export default CustomerSingUp
+export default CustomerSingUp;
--- src/components/Auth/LogIn.jsx
+++ src/components/Auth/LogIn.jsx
@@ -3,7 +3,8 @@ import {NavLink} from 'react-router-dom'
 function Login() {
   return (
     <div>
-        <h1 className='font-bold text-4xl'>Login</h1>
+            <h1 className="uppercase text-center  font-bold text-6xl">our awesome login page</h1>
+
         <div className="home-links | flex">
             <NavLink  className="capitalize"to="/farmerSingUp">farmer sign up</NavLink>
             <NavLink  className="capitalize"to="/customerSingUp">customer sign up</NavLink>
