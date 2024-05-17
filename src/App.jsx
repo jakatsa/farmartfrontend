2dd9fc488319b03be243df95b45c8d9a3ee09e2e
Fuego-fuego <61972879+Fuego-fuego@users.noreply.github.com>
2024-05-17 17:12:49 +0300

    router added (#3)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import './App.css'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import CustomerSingUp from './components/Auth/CustomerSingUp'
+import FarmersSingUp from './components/Auth/FarmersSingUp'
+import LandingPage from './components/LandingPage'
+import Login from './components/Auth/Login'
 
 
 function App() {
 
 
   return (
-    <>
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route exact path="/" element={<LandingPage/>}/>
+      <Route path="/customerSingUp" element={<CustomerSingUp/>}/>
+      <Route path="/farmerSingUp" element={<FarmersSingUp/>}/>      
+      <Route path="/Login" element={<Login/>}/>      
+    </Routes>
+
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/Auth/CustomerSingUp.jsx
@@ -0,0 +1,11 @@
+import {Link} from "react-router-dom"
+function  CustomerSingUp () {
+  return (
+    <div>
+        <h1>CustomerSingUp</h1>
+        <Link to="/login"></Link>        
+    </div>
+  )
+}
+
+export default CustomerSingUp
