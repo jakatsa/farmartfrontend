d3c30d8e3026438153e9058e7a34314ee75114f7
jakatsa <118554412+jakatsa@users.noreply.github.com>
2024-05-18 14:03:43 +0300

    Dev (#7)
    * tailwind install
    
    * Set up Redux store and integrated with Vite project/joe
    
    * NavBar addition/joe

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,31 @@
 
-
-import './App.css'
-import { BrowserRouter, Routes, Route} from 'react-router-dom'
-import CustomerSingUp from './components/Auth/CustomerSingUp'
-import FarmersSingUp from './components/Auth/FarmersSingUp'
-import LandingPage from './components/LandingPage'
-
-import Login from './components/Auth/LogIn'
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CustomerSingUp from "./components/Auth/CustomerSingUp";
+import FarmersSingUp from "./components/Auth/FarmersSingUp";
+import LandingPage from "./components/LandingPage";
 
 
 
+
+
+import Login from "./components/Auth/LogIn";
+import { NavBar } from "./components/NavBar";
 
 function App() {
-
-
   return (
     <BrowserRouter>
-    <Routes>
-      <Route exact path="/" element={<LandingPage/>}/>
-      <Route path="/customerSingUp" element={<CustomerSingUp/>}/>
-      <Route path="/farmerSingUp" element={<FarmersSingUp/>}/>      
+      <NavBar />
+      <Routes>
+        <Route exact path="/" element={<LandingPage />} />
+        <Route path="/customerSingUp" element={<CustomerSingUp />} />
+        <Route path="/farmerSingUp" element={<FarmersSingUp />} />
 
-      <Route path="/login" element={<Login/>}/>      
-
-
-    </Routes>
-
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
 
+export default App;
