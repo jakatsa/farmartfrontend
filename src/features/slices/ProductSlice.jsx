22dfb814b9ef3354565a8bd90b2203a0fa843be1
jakatsa <118554412+jakatsa@users.noreply.github.com>
2024-05-21 12:37:54 +0300

    Dev (#14)
    * tailwind install
    
    * Set up Redux store and integrated with Vite project/joe
    
    * NavBar addition/joe
    
    * homepage creation/joe
    
    * landingpage updates
    
    * filtered products/joe
    
    * filtered products
    
    * filtered products
    
    * filtered products/joe
    
    * filtered products/joe

---
+++ src/features/slices/ProductSlice.jsx
@@ -0,0 +1,41 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { storeData } from "../../assets/data/DummyData";
+export const productSlice = createSlice({
+  name: "product",
+  initialState: {
+    filteredProducts:
+      JSON.parse(sessionStorage.getItem("filteredData")) || storeData,
+    /** 
+    singleProduct:
+      JSON.parse(sessionStorage.getItem("singleProduct")) || storeData,
+    error: false,*/
+  },
+  reducers: {
+    filterProducts(state, action) {
+      try {
+        const filter = storeData.filter(
+          (product) => product.type === action.payload
+        );
+        state.filteredProducts = filter;
+        console.log("filter", filter);
+        state.error = false;
+        const savedState = JSON.stringify(filter);
+        sessionStorage.setItem(
+          "filteredData",
+          savedState
+        ); /**we can also use local storage */
+      } catch (err) {
+        return err;
+      }
+    },
+  },
+});
+export const {
+  filterProducts,
+  /** * singleProduct,
+  filterGender,
+  sortByPrice,
+  filterByColor,
+  filterBySize,*/
+} = productSlice.actions;
+export default productSlice.reducer;
