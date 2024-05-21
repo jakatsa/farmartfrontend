eae2ffa01ea89beb98f916e2fa709e69b795b5a5
terence <terenceshawn18@outlook.com>
2024-05-21 13:59:01 +0300

    added handle submit

--- src/components/Auth/FarmersSingUp.jsx
+++ src/components/Auth/FarmersSingUp.jsx
@@ -13,6 +13,44 @@ function FarmersSingUp() {
         },
         contact: ''
       });
+      const handleChange = (e) => {
+        const { name, value } = e.target;
+        if (name.startsWith('user.')) {
+          const key = name.split('.')[1];
+          setFormData(prevState => ({
+            ...prevState,
+            user: {
+              ...prevState.user,
+              [key]: value
+            }
+          }));
+        } else {
+          setFormData(prevState => ({
+            ...prevState,
+            [name]: value
+          }));
+        }
+      };
+      const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+          const response = await fetch('http://localhost:8000/api/farmer/register/', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formData)
+          });
+          if (!response.ok) {
+            throw new Error('Failed to register');
+          }
+          // If signup is successful, navigate to the login page
+          navigate('/login');
+        } catch (error) {
+          console.error('Error registering:', error);
+        }
+      };
+    
     return <div className="farmer-sign-up | form-wrapper">        
         <div className=" form-wrapper-sign-up | glass flow">
         {/* Logo */}
