7fcc356cc92f9a8f3db4c0775335ec7c09855693
terence <terenceshawn18@outlook.com>
2024-05-21 12:42:05 +0300

    added states

--- src/components/Auth/CustomerSingUp.jsx
+++ src/components/Auth/CustomerSingUp.jsx
@@ -1,2 +1,17 @@
 import React, { useState } from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
+
+
+function CustomerSignUp() {
+    const navigate = useNavigate();
+    const [formData, setFormData] = useState({
+        user: {
+          username: '',
+          email: '',
+          password: '',
+          role: 'customer'
+        }
+      });
+
+
+}
