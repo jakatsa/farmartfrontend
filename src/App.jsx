a42aa7fdf7e0e20dfba20354d213d2c08ae3aa65
nathaniEL mujesia <nathanielmujesia@gmail.com>
2024-05-23 08:58:52 +0300

    Landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,40 @@
 
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import CustomerSingUp from "./components/Auth/CustomerSingUp";
 import FarmersSingUp from "./components/Auth/FarmersSingUp";
 import LandingPage from "./components/LandingPage/LandingPage";
-
-
-
-
-
 import Login from "./components/Auth/LogIn";
-// import { NavBar } from "./components/NavBar";
 
+import { FilteredProducts } from "./components/FilteredProducts/FilteredProducts";
+import { ProductCard } from "./components/FilteredProducts/ProductCard";
+import { NavBar } from "./components/NavBar";
 import { HomePage } from "./components/HomePage";
 import SignUp from "./components/Auth/SignUp";
 
 
 function App() {
   return (
-    <BrowserRouter>
-      {/* <NavBar /> */}
-      <Routes>
-        <Route exact path="/" element={<LandingPage />} />
-        <Route path="/customerSignUp" element={<CustomerSingUp />} />
-        <Route path="/farmerSignUp" element={<FarmersSingUp />} />
-        <Route path="/HomePage" element={<HomePage />} />
-        <Route path="/signup" element={<SignUp/>} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+          <NavBar />
+          <Routes>
+              <Route exact path="/" element={<LandingPage />} />
+              <Route path="/customerSignUp" element={<CustomerSingUp />} />
+              <Route path="/farmerSignUp" element={<FarmersSingUp />} />
+              <Route path="/HomePage" element={<HomePage />} />
+              <Route path="/signup" element={<SignUp />} />
+              <Route path="/login" element={<Login />} />
+              <Route
+                  path="/FilteredProducts/:type"
+                  element={<FilteredProducts />}
+              />
+              <Route
+                  path="/FilteredProducts"
+                  element={<Navigate to="/FilteredProducts/default" />}
+              />
+              <Route path="/ProductCard" element={<ProductCard />} />
+          </Routes>
+      </BrowserRouter>
   );
 }
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer () {
         <div className="container">
             <div className="footer-grid grid">
                 {/* Trade Assurance */}
-                <section className="trade | footer-nav-block">
+                <section className=" trade | footer-nav-block">
                     <h2 className="footer-heading | font-semibold capitalize">trade assurance</h2>
                     <nav>
                         <ul className="footer-nav-list | flex flex-col">
@@ -47,10 +47,10 @@ function Footer () {
                 {/* Social links */}
                 <nav className="social-links" aria-label="social links">
                     <ul className="footer-social-links-list flex">
-                        <li><img src="../src/assets/icons/icon-facebook.svg" alt="facebook icon" className="footer-social-icon" /></li>
-                        <li><img src="../src/assets/icons/icon-youtube.svg" alt="youtube icon" className="footer-social-icon" /></li>
-                        <li><img src="../src/assets/icons/icon-instagram.svg" alt="instagram icon" className="footer-social-icon" /></li>
-                        <li><img src="../src/assets/icons/icon-pinterest.svg" alt="pinterest icon" className="footer-social-icon" /></li>
+                        <li className="social-link"><img src="../src/assets/icons/icon-facebook.svg" alt="facebook icon" className="footer-social-icon" /></li>
+                        <li className="social-link"><img src="../src/assets/icons/icon-youtube.svg" alt="youtube icon" className="footer-social-icon" /></li>
+                        <li className="social-link"><img src="../src/assets/icons/icon-instagram.svg" alt="instagram icon" className="footer-social-icon" /></li>
+                        <li className="social-link"><img src="../src/assets/icons/icon-pinterest.svg" alt="pinterest icon" className="footer-social-icon" /></li>
                     </ul>
                 </nav>
             </div>
