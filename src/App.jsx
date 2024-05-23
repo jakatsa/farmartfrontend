7332486239d8177027fa33edd29fc080a1b963ad
nathaniEL mujesia <nathanielmujesia@gmail.com>
2024-05-23 09:05:54 +0300

    solving merge conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ import FarmersSingUp from "./components/Auth/FarmersSingUp";
 import LandingPage from "./components/LandingPage/LandingPage";
 import Login from "./components/Auth/LogIn";
 
-import { FilteredProducts } from "./components/FilteredProducts/FilteredProducts";
-import { ProductCard } from "./components/FilteredProducts/ProductCard";
+// import { FilteredProducts } from "./components/FilteredProducts/FilteredProducts";
+// import { ProductCard } from "./components/FilteredProducts/ProductCard";
 import { NavBar } from "./components/NavBar";
 import { HomePage } from "./components/HomePage";
 import SignUp from "./components/Auth/SignUp";
@@ -24,15 +24,15 @@ function App() {
               <Route path="/HomePage" element={<HomePage />} />
               <Route path="/signup" element={<SignUp />} />
               <Route path="/login" element={<Login />} />
-              <Route
+              {/* <Route
                   path="/FilteredProducts/:type"
                   element={<FilteredProducts />}
-              />
-              <Route
+              /> */}
+              {/* <Route
                   path="/FilteredProducts"
                   element={<Navigate to="/FilteredProducts/default" />}
-              />
-              <Route path="/ProductCard" element={<ProductCard />} />
+              /> */}
+              {/* <Route path="/ProductCard" element={<ProductCard />} /> */}
           </Routes>
       </BrowserRouter>
   );
