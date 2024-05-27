babeba9915bd8618739b1c56c9e10dadc2610b96
terence <terenceshawn18@outlook.com>
2024-05-27 16:08:09 +0300

    changes

--- src/components/SingleProduct.jsx
+++ src/components/SingleProduct.jsx
@@ -9,10 +9,6 @@ export const SingleProduct = () => {
   const [error, setError] = useState(null);
   const location = useLocation();
   const { id } = useParams();
-  const storedToken = localStorage.getItem("token");
-  const customerId = localStorage.getItem("customer_id"); // Assuming customer ID is stored in localStorage
-  const quantity = 1; // Default quantity, you can adjust this as needed
-
   console.log(id);
 
   useEffect(() => {
@@ -26,7 +22,7 @@ export const SingleProduct = () => {
     const fetchAnimals = async () => {
       try {
         const response = await fetch(
-          `http://127.0.0.1:8000/api/animals/${id}`
+          `https:farmartbackend-3.onrender.com/api/animals/${id}`
         );
         if (!response.ok) throw new Error("Network response was not ok");
         const data = await response.json();
@@ -43,33 +39,6 @@ export const SingleProduct = () => {
     }
   }, [id, location.state]);
 
-  const addToCart = async () => {
-    try {
-      const response = await fetch("http://localhost:8000/api/orders/create/", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Token ${storedToken}`,
-        },
-        body: JSON.stringify({
-          customer: customerId,
-          animal: animal.id,
-          quantity: quantity,
-        }),
-      });
-      const data = await response.json();
-      if (response.ok) {
-        console.log("Order created successfully:", data);
-        alert("Order created successfully!");
-      } else {
-        console.error("Error:", data);
-        alert("Failed to create order.");
-      }
-    } catch (error) {
-      console.error("Error:", error);
-      alert("Failed to create order.");
-    }
-  };
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error}</p>;
@@ -95,7 +64,6 @@ export const SingleProduct = () => {
             </div>
             <button
               className="bg-blue-500 text-white px-4 py-2 rounded-lg flex items-center gap-2 hover:bg-blue-600 transition duration-300"
-              onClick={addToCart}
             >
               Add To Cart
             </button>
